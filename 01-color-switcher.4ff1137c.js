6e43257dfb81fd9371720c98fd92e1cc66ecea77
DymitrDworakowski <116100679+DymitrDworakowski@users.noreply.github.com>
2023-04-19 13:56:07 +0000

    Deploying to gh-pages from @ DymitrDworakowski/goit-js-hw-09@181ab829ff0e1bb80127ae6f183434ad6a386a6d 🚀

---
+++ 01-color-switcher.4ff1137c.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");let o=null;t.addEventListener("click",(()=>{o=setInterval((()=>{const t=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;document.body.style.backgroundColor=t}),1e3)})),e.addEventListener("click",(()=>{clearInterval(o),console.log("Interval has stopped!")}));
+//# sourceMappingURL=01-color-switcher.4ff1137c.js.map
