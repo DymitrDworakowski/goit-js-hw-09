181ab829ff0e1bb80127ae6f183434ad6a386a6d
DymitrDworakowski <116100679+DymitrDworakowski@users.noreply.github.com>
2023-04-19 15:55:42 +0200

    all

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,20 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+
+function getRandomHexColor() {
+const paddedHexNumber = Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0);
+return `#${paddedHexNumber}`;
+}
+let timerId = null;
+startBtn.addEventListener("click", () => {
+    timerId = setInterval(() => {
+        const color = getRandomHexColor();
+    document.body.style.backgroundColor =color;
+}, 1000);
+});
+
+stopBtn.addEventListener("click", () => {
+clearInterval(timerId);
+console.log(`Interval has stopped!`);
+});
 
